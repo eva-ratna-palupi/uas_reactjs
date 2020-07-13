d681722bd4da357d7617ce254ba8b7bbf1e972c9
Eva Ratna Palupi <evaratnapalupi@outlook.com>
2020-07-13 19:12:36 +0700

    Menambahkan desain home

--- src/Component/Class/BootstrapComp.jsx
+++ src/Component/Class/BootstrapComp.jsx
@@ -20,7 +20,14 @@ class BootstrapComp extends Component {
                     <h1 className="display-4"><img src={logo} className="App-logo" alt="logo" /></h1>
                     <p className="text-info">Created by: Eva Ratna Palupi</p>
                 </header>
+                <br/><br/><br/>
+                <div className="card-body">
+                    <h5 className="card-title">Hallo kamu sudah punya akun belum?</h5>
+                    <p className="card-text">Kalau sudah klik Login dan kalau belum Daftar dulu yuk</p>
+                    <a href="#" className="btn btn-primary">Login</a> <a href="#" className="btn btn-success">Daftar</a>
+                </div>
             </div>
+
 
 
         )
