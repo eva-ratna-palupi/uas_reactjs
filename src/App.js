368a91e7cbe21e496f1d3ef32d450fdd82bbffd8
Eva Ratna Palupi <evaratnapalupi@outlook.com>
2020-07-20 15:18:36 +0700

    Manambahkan package axios

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import NavbarComp from '../src/Component/Fungsional/NavbarComp'
 import Home from '../src/Component/Fungsional/Home'
 import ListComp from './Component/Class/ListComp';
