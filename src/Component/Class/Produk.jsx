7bfac1a1ad12b2b8d58404afd29f6998e8b000e5
Eva Ratna Palupi <evaratnapalupi@outlook.com>
2020-07-21 01:49:28 +0700

    Memperbaiki ui produk

--- src/Component/Class/Produk.jsx
+++ src/Component/Class/Produk.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './CSS/Produk.css'
+import { Button, Card, Col, CardTitle, Row, CardBody, CardImg } from 'reactstrap'
 
 
 class Produk extends React.Component {
@@ -29,15 +30,19 @@ class Produk extends React.Component {
 
     render() {
         return (
-            <div className="box-stock">
-                    <h2 className="teks">{this.props.nama}</h2>
-                    <img src={this.props.img} alt="" />
-                    <p><b>Rp. {this.props.harga} /kg</b></p>
-                    <p>Stok : {this.state.stock}</p>
-                    <button className="btn-click" onClick={this.ButtonBeli} disabled={this.state.disabled}>Beli</button>
-                    <p>{this.state.status}</p>
-                
-               </div>
+            <div>
+                <Row>
+                    <Col sm="3">
+                        <Card body>
+                            <CardTitle>{this.props.nama}</CardTitle>
+                        <img width="100%" src={this.props.img} alt="" />
+                        <p><b>Rp. {this.props.harga} /kg</b></p>
+                            <p>Stok : {this.state.stock}</p>
+                            <p><Button outline color="success" className="btn-click" onClick={this.ButtonBeli} disabled={this.state.disabled}>Beli</Button>        {this.state.status}</p>
+                            </Card>
+            </Col>
+            </Row>
+            </div >
 
         )
     }
--- src/Component/Fungsional/Home.jsx
+++ src/Component/Fungsional/Home.jsx
@@ -4,40 +4,121 @@ import anggur from '../Gambar/anggur.jpg'
 import apel from '../Gambar/apple.jpg'
 import avocado from '../Gambar/avocado.jpg'
 import banana from '../Gambar/banana.jpg'
-import {Container} from 'reactstrap'
+import bery from '../Gambar/berry.jpg'
+import dragon from '../Gambar/dragon-fruit.jpg'
+import jeruk from '../Gambar/jeruk.jpg'
+import kiwi from '../Gambar/kiwi.jpg'
+import lemon from '../Gambar/lemon.jpg'
+import mangga from '../Gambar/manggo.jpg'
+import pepaya from '../Gambar/papaya.jpg'
+import nanas from '../Gambar/pineapple.jpg'
+import strawbery from '../Gambar/strawberries.jpg'
+import semangka from '../Gambar/watermelon.jpg'
+
+import { Container } from 'reactstrap'
 
 
 const Home = () => {
     return <div>
         <Container className="themed-container" fluid={true}>
-        <br/>
-        <Produk
-            stok="10"
-            nama="Anggur California"
-            harga="35.500"
-            img={anggur}
-        />
-        
-        <Produk
-            stok="15"
-            nama="Apel Hijau Malang"
-            harga="25.000"
-            img={apel}
-        />
+            <br />
+            <Produk
+                stok="10"
+                nama="Anggur California"
+                harga="35.500"
+                img={anggur}
+            />
 
-        <Produk
-            stok="20"
-            nama="Alpukat"
-            harga="30.500"
-            img={avocado}
-        />
+            <Produk
+                stok="15"
+                nama="Apel Hijau Malang"
+                harga="25.000"
+                img={apel}
+            />
 
-        <Produk
-            stok="30"
-            nama="Pisang Ambon"
-            harga="20.000"
-            img={banana}
-        />
+            <Produk
+                stok="20"
+                nama="Alpukat"
+                harga="30.500"
+                img={avocado}
+            />
+
+            <Produk
+                stok="30"
+                nama="Pisang Ambon"
+                harga="20.000"
+                img={banana}
+            />
+
+            <Produk
+                stok="30"
+                nama="Berry Ungu"
+                harga="20.000"
+                img={bery}
+            />
+
+            <Produk
+                stok="30"
+                nama="Buah Naga"
+                harga="20.000"
+                img={dragon}
+            />
+
+            <Produk
+                stok="30"
+                nama="Jeruk Mandarin"
+                harga="20.000"
+                img={jeruk}
+            />
+
+            <Produk
+                stok="30"
+                nama="Kiwi "
+                harga="20.000"
+                img={kiwi}
+            />
+
+            <Produk
+                stok="30"
+                nama="Lemon"
+                harga="20.000"
+                img={lemon}
+            />
+
+            <Produk
+                stok="30"
+                nama="Mangga"
+                harga="20.000"
+                img={mangga}
+            />
+
+            <Produk
+                stok="30"
+                nama="Pepaya "
+                harga="20.000"
+                img={pepaya}
+            />
+
+            <Produk
+                stok="30"
+                nama="Nanas"
+                harga="20.000"
+                img={nanas}
+            />
+
+            <Produk
+                stok="30"
+                nama="Strawberry"
+                harga="20.000"
+                img={strawbery}
+            />
+
+            <Produk
+                stok="30"
+                nama="Semangka"
+                harga="20.000"
+                img={semangka}
+            />
         </Container>
     </div>
 }
