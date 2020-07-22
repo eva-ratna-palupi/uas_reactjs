f8ac2894365589b4ad3cc2217b508c5900e92c20
Eva Ratna Palupi <evaratnapalupi@outlook.com>
2020-07-22 21:19:20 +0700

    Membuat file register

---
+++ src/Component/Fungsional/Register.jsx
@@ -0,0 +1,11 @@
+import React, {Fragment} from 'react';
+
+const Register = () => {
+    return (
+        <div>
+            <Fragment>REGISTER</Fragment>
+        </div>
+    );
+}
+
+export default Register;
